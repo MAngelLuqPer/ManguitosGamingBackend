cb9adb87d7b10d65430d5e13b32dc5f2ef8c9e94
MAngelLuqPer <luque.perez.miguel.angel@iescamas.es>
2025-05-06 16:07:07 +0200

    Añadir funciones nuevas a controladores

---
+++ proyectofct/src/java/model/entities/DTOs/UnionDTO.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.entities.DTOs;
+
+/**
+ *
+ * @author mangel
+ */
+public class UnionDTO {
+    public Long comunidadId;
+    public Long usuarioId;
+
+    public UnionDTO() {}
+
+    public Long getComunidadId() {
+        return comunidadId;
+    }
+
+    public void setComunidadId(Long comunidadId) {
+        this.comunidadId = comunidadId;
+    }
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+    
+    
+}
--- proyectofct/src/java/rs/ComunidadREST.java
+++ proyectofct/src/java/rs/ComunidadREST.java
@@ -4,16 +4,25 @@
  */
 package rs;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import model.entities.Comunidad;
 import model.entities.DTOs.ComunidadDTO;
+import model.entities.DTOs.UnionDTO;
+import model.entities.Usuario;
 import model.services.ComunidadService;
+import model.services.UsuarioService;
 
 /**
  *
@@ -23,6 +32,7 @@ import model.services.ComunidadService;
 public class ComunidadREST {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("proyectofctPU");
     ComunidadService cs = new ComunidadService(emf);
+    UsuarioService us = new UsuarioService(emf);
     @GET
     public Response getAllComunidades() {
     List<Comunidad> comunidades = cs.findComunidadEntities();
@@ -45,6 +55,44 @@ public class ComunidadREST {
         return Response.ok(comunidadDTO).build();
 
     }
-    
+    @POST
+    @Path("/unir")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response joinComunidad (UnionDTO unionDTO) throws Exception {
+        Usuario usu = us.findUsuario(unionDTO.usuarioId);
+        Comunidad com = cs.findComunidad(unionDTO.comunidadId);
+        if (usu == null || com == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Usuario o Comunidad no encontrados").build();
+        }
+        com.getUsuarios().add(usu);
+        usu.getComunidades().add(com);
+        cs.edit(com); 
+        us.edit(usu);
+
+            return Response.status(Response.Status.CREATED).build();
+
+    }
+    @GET
+    @Path("/{comunidadId}/pertenece/{usuarioId}")
+    public Response pertenece(@PathParam("comunidadId") Long comunidadId, @PathParam("usuarioId") Long usuarioId) {
+        Comunidad com = cs.findComunidad(comunidadId);
+        boolean pertenece = com.getUsuarios().stream()
+        .anyMatch(u -> u.getId().equals(usuarioId));
+        return Response.ok(Collections.singletonMap("pertenece", pertenece)).build();
+    }
+    @DELETE
+    @Path("/{comunidadId}/salir/{usuarioId}")
+    public Response salirComunidad (@PathParam("usuarioId") Long usuarioId, @PathParam("comunidadId") Long comunidadId) throws Exception {
+        Comunidad com = cs.findComunidad(comunidadId);
+        Usuario usu = us.findUsuario(usuarioId);
+        if (com == null || usu == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Usuario o Comunidad no encontrados").build();
+        }
+        com.getUsuarios().removeIf(u -> u.getId().equals(usuarioId));
+        usu.getComunidades().removeIf(c -> c.getId().equals(comunidadId));
+        cs.edit(com);
+        us.edit (usu);
+        return Response.status(Response.Status.ACCEPTED).build();
+    }
 }
 
